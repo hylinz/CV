3b49bac788d36937f36c3a0bd8526d6b6bb64b36
big easy <vandecarl@gmail.com>
2024-04-24 20:07:39 +0200

    refolder

---
+++ components/ui/mode-toggle.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { SunIcon, MoonIcon } from "lucide-react";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => {
+        if (theme === "dark") {
+          setTheme("light");
+        } else {
+          setTheme("dark");
+        }
+      }}
+    >
+      {theme && theme === "dark" ? <SunIcon /> ?? <MoonIcon /> : <MoonIcon />}
+    </Button>
+  );
+}
